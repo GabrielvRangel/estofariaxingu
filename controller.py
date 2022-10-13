52d9889b36b780f8de11440300201e90df88a7e8
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-10-13 13:23:52 -0300

    Deploy final v1

--- controller.py
+++ controller.py
@@ -1,18 +1,18 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from sqlalchemy import Time, create_engine
 import pandas as pd
 import model
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 dash = model.Dashboard()
+agenda = model.Slots()
 regiões = dash.opçãodefiltroregião()
 bus = dash.opçãodefiltrobu()
 
 @app.route("/")
 def index():    
-    regiões = dash.opçãodefiltroregião()
-    bus = dash.opçãodefiltrobu()
     return render_template("index.html", regiões = regiões, bus = bus)
 
 @app.route("/filtrar", methods=["GET","POST"])
@@ -25,6 +25,19 @@ def filtrar():
     if not date or região == "Escolha a região" or bu == "Escolha o produto":
         return render_template("index.html", regiões= regiões, bus= bus)
     else:
+        capacidade = dash.tratarfiltrarcapacidade(date, região, bu)
+        capacidadeheading = list(capacidade)
+        capacidadestatus = list(capacidade['status'])
+        capacidadedate1 = list(capacidade[str(dash.somardata(date, 0))])
+        capacidadedate2 = list(capacidade[str(dash.somardata(date, 1))])
+        capacidadedate3 = list(capacidade[str(dash.somardata(date, 2))])
+        capacidadedate4 = list(capacidade[str(dash.somardata(date, 3))])
+        capacidadedate5 = list(capacidade[str(dash.somardata(date, 4))])
+        capacidadedate6 = list(capacidade[str(dash.somardata(date, 5))])
+        capacidadedate7 = list(capacidade[str(dash.somardata(date, 6))])
+        capacidadedate8 = list(capacidade[str(dash.somardata(date, 7))])
+        capacidadedate9 = list(capacidade[str(dash.somardata(date, 8))])
+        capacidadedate10 = list(capacidade[str(dash.somardata(date, 9))])
         prioridade = dash.tratarfiltrarprioridade(date, região, bu)
         prioridadeheading = list(prioridade)
         prioridaderegião = list(prioridade['região'])
@@ -40,40 +53,75 @@ def filtrar():
         prioridadedate8 = list(prioridade[str(dash.somardata(date, 7))])
         prioridadedate9 = list(prioridade[str(dash.somardata(date, 8))])
         prioridadedate10 = list(prioridade[str(dash.somardata(date, 9))])
-
         escala = dash.filtrarescala(date, região, bu)
         escalaheading = list(escala)
         escalaregião = list(escala['região']) 
         escalahub = list(escala['hub'])
         escalaescala = list(escala['escala'])
         escaladata = list(escala['data'].astype(str))
-        escalaid_colaborador = list(escala['id_colaborador'])
-        escalacolaborador = list(escala['colaborador'])
-        escalaid_cargo = list(escala['id_cargo'])
-        escaladatainicio = list(escala['data_inicio'])
-        escaladatafim = list(escala['data_fim'])
-        escalaausente = list(escala['ausente_lancamento'])
+        escalaid_técnica = list(escala['id_técnica'])
+        escalatécnica = list(escala['técnica'])
+        escalahrentrada = list(escala['hr_entrada'])
+        escalahrsaída = list(escala['hr_saída'])
+        escalaárea = list(escala['área'])
         escalabu = list(escala['bu'])
+        escalastatus = list(escala['status'])
         return  render_template("index.html", prioridadeheading=prioridadeheading, prioridaderegião=prioridaderegião, prioridadeárea=prioridadeárea, prioridadehub=prioridadehub, prioridadedate1=prioridadedate1, prioridadedate2=prioridadedate2, prioridadedate3=prioridadedate3,
         prioridadedate4=prioridadedate4, prioridadedate5=prioridadedate5, prioridadedate6=prioridadedate6, prioridadedate7=prioridadedate7, 
         prioridadedate8=prioridadedate8, prioridadedate9=prioridadedate9, prioridadedate10=prioridadedate10,
         escalaheading=escalaheading, escalaregião=escalaregião, escalahub=escalahub, escalaescala=escalaescala, 
-        escaladata=escaladata, escalaid_colaborador=escalaid_colaborador, escalacolaborador=escalacolaborador, escalaid_cargo=escalaid_cargo, 
-        escaladatainicio=escaladatainicio, escaladatafim=escaladatafim, escalaausente=escalaausente, escalabu=escalabu, regiões = regiões, bus = bus)
+        escaladata=escaladata, escalaid_técnica=escalaid_técnica, escalatécnica=escalatécnica,
+        escalahrentrada=escalahrentrada, escalahrsaída=escalahrsaída, escalaárea=escalaárea, escalabu=escalabu, escalastatus=escalastatus, regiões = regiões, bus = bus,
+        capacidadeheading=capacidadeheading, capacidadestatus=capacidadestatus, capacidadedate1=capacidadedate1, capacidadedate2=capacidadedate2, capacidadedate3=capacidadedate3, 
+        capacidadedate4=capacidadedate4, capacidadedate5=capacidadedate5, capacidadedate6=capacidadedate6, capacidadedate7=capacidadedate7, capacidadedate8=capacidadedate8, 
+        capacidadedate9=capacidadedate9, capacidadedate10=capacidadedate10)
 
 @app.route("/abrirslots")
-def abrirslots():    
+def abrirslots():   
     data = request.args.get('col')
     área = request.args.get('lin')
-    tabela = request.args.get('cel')
-    print('////')
-    print(data)
-    print('////')
-    print(área)
-    print('////')
-    print(tabela)
-    print('////')
-    return render_template("index.html")
+    status = request.args.get('status')
+    id_técnica = request.args.get('id_técnica')
+    técnica = request.args.get('técnica')
+    produto = request.args.get('produto')
+    regime = request.args.get('regime')
+    inicioregime = request.args.get('inicioregime')
+    fimregime = request.args.get('fimregime')
+    hub = request.args.get('hub')
+    duração = dash.duraçãoslot(hub, produto)
+    regime = dash.regime(regime)
+    idparceiro = dash.idparceiro(área)
+    slotatual = datetime.strptime(inicioregime, "%H:%M:%S")
+    print('Você está abrindo slot na área: ' + área + '.')
+    print(' O ID do parceiro da área é: ' + idparceiro + '.')
+    if regime == 'rotating': 
+        while(slotatual < datetime.strptime(fimregime, "%H:%M:%S") - timedelta(hours=0, minutes=duração, seconds=0)):
+            slotatual = slotatual + timedelta(hours=0, minutes=duração, seconds=0)
+            if slotatual >= datetime.strptime('12:00:00', "%H:%M:%S") and slotatual < datetime.strptime('13:00:00', "%H:%M:%S"):
+                slotatual = datetime.strptime('13:00:00', "%H:%M:%S")
+            slotatualtexto = slotatual.strftime('%H:%M:%S')
+            if slotatualtexto != "19:00:00":    
+                print('Abrindo slot ' + slotatualtexto + '...')
+                token = dash.token()
+                agenda.abrirslots(f'{data}', f'{regime}', f'{produto}', idparceiro, slotatualtexto, token)
+                id = agenda.iddoslot()
+                dash.inserirdados( id, data, slotatual, área, hub, regime, produto, id_técnica, técnica)
+
+    elif regime == 'diarist':
+        while(slotatual < datetime.strptime(fimregime, "%H:%M:%S") - timedelta(hours=0, minutes=duração, seconds=0)):
+            slotatual = slotatual + timedelta(hours=0, minutes=duração, seconds=0)
+            slotatualtexto = slotatual.strftime('%H:%M:%S')
+            print('Abrindo slot ' + slotatualtexto + '...')
+            token = dash.token()
+            agenda.abrirslots(f'{data}', f'{regime}', f'{produto}', idparceiro, slotatualtexto, token)
+            id = agenda.iddoslot()
+            dash.inserirdados( id, data, slotatual, área, hub, regime, produto, id_técnica, técnica)
+
+    print('Todos os slots abertos com sucesso!')
+    return redirect("http://127.0.0.1:5000/", code=302)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug= True)
--- model.py
+++ model.py
@@ -4,37 +4,47 @@ import pandas as pd
 import datetime
 import requests
 import os
-class Agenda():
-    def __init__(self, hub, nome, regime, horárioinicial, horáriofinal, bu, alocação_disponível, data):
-        self.hub = hub
-        self.nome = nome
-        self.regime = regime
-        self.horárioinicial = horárioinicial
-        self.horáriofinal = horáriofinal
-        self.bu = bu
-        self.alocação_disponível = alocação_disponível
-        self.data = data
+import json
+class Slots():
+    def abrirslots(self, data, regime, bu, idparceiro, slot_hora, urltoken):    
+        payload = {
+            "bookings": [
+                {
+                    "date": f"{data}", # Data da agenda
+                    "work_shift": f"{regime}", # tipo de regime (diarist = Diarista | rotating = Plantonista)
+                    "product_type": f"{bu}", # Tipo de produto da agenda (vaccines = Vacinas | laboratories = Exames) 
+                    "supplier_id": idparceiro, # ID da região onde a agenda será alocada
+                    "slots": [
+                        { "time": f"{slot_hora}", "supplier_id": idparceiro, "duration": 40 }
+                    ] # lista de slots seguindo a estrutura, Opcional
+                }
+            ]
+        }
         
-    def filtraragenda():
-        print("filtra uma agenda")
-    def verificardisponibilidade():
-        print("verificando se a alocação está disponível")
-    def escolherparceiro():
-        print("Selecionar parceiro para abrir os slots")
-    def alocaragenda():
-        print("abrindo slots para agenda x filtrada")
+        response = requests.post(url=urltoken, json=payload)
+        self.dados = json.loads(response.text)
+        self.dados2 = self.dados[0]
+        self.dados3 = self.dados2['slots']
+        self.dados4 = self.dados3[0]
+        self.slotid = self.dados4['id']
+    
+    def iddoslot(self):
+        return self.slotid
 
+       
 class Dashboard():    
     def __init__(self):
         usuario =  os.environ['usuario']
         senha =  os.environ['senha']
         servidor =  os.environ['servidor']
         banco =  os.environ['banco']
+        sp_usuario =  os.environ['sp_usuario']
+        sp_senha =  os.environ['sp_senha']
+        sp_servidor =  os.environ['sp_servidor']
+        sp_banco =  os.environ['sp_banco']
         self.conexão = create_engine(f"""postgresql://{usuario}:{senha}@{servidor}/{banco}""")
+        self.serverproduction = create_engine(f"""postgresql://{sp_usuario}:{sp_senha}@{sp_servidor}/{sp_banco}""")
 
-    def tratarcards():
-        print("Tratando os cards")
-    
     def tratarfiltrarprioridade(self, data, região, bu):
         consulta = f"""
         select macro_região as região, a.hub, a.área, max(a.score) as score, a.data from ( 
@@ -57,39 +67,60 @@ class Dashboard():
         self.prioridadetratada.columns = ['região', 'hub', 'área', str(self.somardata(data, 0)), str(self.somardata(data, 1)), str(self.somardata(data, 2)), str(self.somardata(data, 3)), str(self.somardata(data, 4)), str(self.somardata(data, 5)), str(self.somardata(data, 6)), str(self.somardata(data, 7)), str(self.somardata(data, 8)), str(self.somardata(data, 9))] 
         return self.prioridadetratada
 
-    def tratarcapacidade():
-        print("Tratando tabela com capacidade, alocado e saldo.")
-    
+    def tratarfiltrarcapacidade(self, data, região, bu):
+        consulta = f"""    
+        select a.status, count(a.status) as quant, a.data from (
+        select macro_região as região, jeeo.hub, jeeo.escala, jeeo.data, jeeo.id_colaborador as id_técnica, jeeo.colaborador as técnica, jeeo.data_inicio_previsto::time as hr_entrada, jeeo.data_fim_previsto::time as hr_saída, 
+        wsa."area" as área,
+        (case when jeeo.escala LIKE '%VAC%' then 'vaccines' when jeeo.escala LIKE '%LAB%' then 'laboratories' else 'híbrida' end) as bu,
+        (case when wsa.tecnica is not null then 'Ocupado' else 'Disponível' end) as status
+        from jornadas_escala.escala_operacional jeeo
+        left join workstation.slots_abertos wsa
+        on concat(wsa.data::text, wsa.id_tecnica::text) = concat(jeeo.data::text, jeeo.id_colaborador::text) 
+        left join dim_parceiros
+        on "HUB" = jeeo.hub
+        where escala LIKE '%Técnica%'
+        and macro_região = '{região}'
+        and previsto = 'Trabalho'
+        and (lancamento <> 'Afastamento INSS' and lancamento <> 'Treinamento' and lancamento <> 'Licença maternidade' and lancamento <> 'Curso/Evento' and lancamento <> 'Férias' and lancamento <> 'Recesso' and lancamento <> 'Licença nojo/óbito' and lancamento <> 'Atividade administrativa' and lancamento <> 'Folga' and lancamento <> 'Folga extra' and lancamento <> 'Licença gala' or lancamento is null)
+        and jeeo.data > current_date
+        group by  macro_região, jeeo.hub, jeeo.escala, jeeo.data::date, jeeo.id_colaborador, jeeo.colaborador, jeeo.id_cargo, jeeo.data_inicio_previsto, jeeo.data_fim_previsto, wsa.tecnica, wsa."area"
+        order by status, jeeo.data::date, jeeo.hub, jeeo.colaborador ) a 
+        where a.bu = '{bu}'
+        and a.data >= '{data}' and a.data <= to_char(DATE '{data}', 'YYYY/MM/DD')::date + interval '9 days'
+        group by a.status, a.data
+        """
+        self.capacidadetratada = pd.read_sql_query(consulta, con=self.conexão)
+        self.capacidadetratada = pd.pivot_table(self.capacidadetratada, index=["status"], columns=["data"], values=["quant"])
+        self.capacidadetratada = self.capacidadetratada.set_axis(self.capacidadetratada.columns.tolist(), axis=1).reset_index()
+        self.capacidadetratada.columns = ['status', str(self.somardata(data, 0)), str(self.somardata(data, 1)), str(self.somardata(data, 2)), str(self.somardata(data, 3)), str(self.somardata(data, 4)), str(self.somardata(data, 5)), str(self.somardata(data, 6)), str(self.somardata(data, 7)), str(self.somardata(data, 8)), str(self.somardata(data, 9))] 
+        return self.capacidadetratada
+
     def tratarescala(self):
         consulta = f"""
-        select macro_região as região, hub, escala, data, id_colaborador, colaborador, id_cargo, data_inicio_previsto::time as data_inicio, data_fim_previsto::time as data_fim, ausente_lancamento,
-        (case when escala LIKE '%VAC%' then 'vaccines' when escala LIKE '%LAB%' then 'laboratories' else 'híbrida' end) as bu
-        from jornadas_escala.escala_operacional
+        select macro_região as região, jeeo.hub, jeeo.escala, jeeo.data, jeeo.id_colaborador as id_técnica, jeeo.colaborador as técnica, jeeo.data_inicio_previsto::time as hr_entrada, jeeo.data_fim_previsto::time as hr_saída, 
+        wsa."area" as área,
+        (case when jeeo.escala LIKE '%VAC%' then 'vaccines' when jeeo.escala LIKE '%LAB%' then 'laboratories' else 'híbrida' end) as bu,
+        (case when wsa.tecnica is not null then 'Ocupado' else 'Disponível' end) as status
+        from jornadas_escala.escala_operacional jeeo
+        left join workstation.slots_abertos wsa
+        on concat(wsa.data::text, wsa.id_tecnica::text) = concat(jeeo.data::text, jeeo.id_colaborador::text) 
         left join dim_parceiros
-        on "HUB" = hub
+        on "HUB" = jeeo.hub
         where escala LIKE '%Técnica%'
         and previsto = 'Trabalho'
-        and (lancamento = 'Licença médica' or lancamento = 'Folga hora' or lancamento is null)
-        and data > current_date
-        group by  macro_região, hub, escala, data::date, id_colaborador, colaborador, id_cargo, data_inicio_previsto, data_fim_previsto, ausente_lancamento
-        order by data::date, hub, colaborador
+        and (lancamento <> 'Afastamento INSS' and lancamento <> 'Treinamento' and lancamento <> 'Licença maternidade' and lancamento <> 'Curso/Evento' and lancamento <> 'Férias' and lancamento <> 'Recesso' and lancamento <> 'Licença nojo/óbito' and lancamento <> 'Atividade administrativa' and lancamento <> 'Folga' and lancamento <> 'Folga extra' and lancamento <> 'Licença gala' or lancamento is null)
+        and jeeo.data > current_date
+        group by  macro_região, jeeo.hub, jeeo.escala, jeeo.data::date, jeeo.id_colaborador, jeeo.colaborador, jeeo.id_cargo, jeeo.data_inicio_previsto, jeeo.data_fim_previsto, wsa.tecnica, wsa."area"
+        order by status, jeeo.data::date, jeeo.hub, jeeo.colaborador
         """
         self.escalatratada = pd.read_sql_query(consulta, con=self.conexão)
         return self.escalatratada
 
-    def filtrarcards():
-        print("filtrando cards")
-
-    def filtrarcapacidade():
-        print("Filtrando tabela com capacidade, alocado e saldo.")
-    
     def filtrarescala(self, data, região, bu): 
         self.escalatratada['data'] = pd.to_datetime(self.escalatratada['data'])
         filtrarescala = self.escalatratada[(self.escalatratada['data'] >= f'{data}') & (self.escalatratada['data'] <= f'{str(self.somardata(data, 9))}') & (self.escalatratada['região'] == f'{região}') & (self.escalatratada['bu'] == f'{bu}')] 
         return filtrarescala
-
-    def receberopçãodefiltro(self):
-        print('filtrando tudo')
     
     def opçãodefiltroregião(self):
         consulta = f"select macro_região from dim_parceiros where macro_região is not null group by macro_região"
@@ -113,28 +144,50 @@ class Dashboard():
         bus = opçãobu['bu'].values
         return bus
 
-    def opçãodefiltrostatus(self):
-        print("Mostrar se a capacidade está disponível ou alocada")
-
     def somardata(self, data, dias):
         soma = datetime.datetime.strptime(data, '%Y-%m-%d') + datetime.timedelta(days=dias)
         return soma.strftime("%Y-%m-%d")
 
+    def idparceiro(self, parceiro):
+        consulta = f"select parceiro_nome, id_parceiro from dim_parceiros"
+        idparceiro = pd.read_sql_query(consulta, con=self.conexão)
+        idparceiro = idparceiro[(idparceiro['parceiro_nome'] == f'{parceiro}')]
+        idparceiro = int(idparceiro.iloc[0]['id_parceiro'])
+        idparceiro = json.dumps(idparceiro)
+        return idparceiro
 
-# url = f'https://api.beepapp.com.br/api/v8/booking_management/schedule_bookings?session_token={token}'
-# payload = {
-#     "bookings": [
-#         {
-#             "date": "2022-12-27", # Data da agenda
-#             "work_shift": "diarist", # tipo de regime (diarist = Diarista | rotating = Plantonista)
-#             "product_type": "vaccines", # Tipo de produto da agenda (vaccines = Vacinas | laboratories = Exames) 
-#             "supplier_id": 634, # ID da região onde a agenda será alocada
-#             "slots": [
-#                 { "time": "08:00", "supplier_id": 634, "duration": 40 }
-#             ] # lista de slots seguindo a estrutura, Opcional
-#         }
-#         ]
-#     }
+    def duraçãoslot(self, hub, bu):
+        if (hub == 'Brasília' and bu == 'vaccines') or (hub == 'São Cristóvão' and bu == 'vaccines') or (hub == 'Barra' and bu == 'vaccines') or (hub == 'Cabo Frio' and bu == 'vaccines') or (hub == 'Alphaville' and bu == 'vaccines') or (hub == 'Vila Olímpia' and bu == 'vaccines') or (hub == 'São Bernardo do Campo' and bu == 'vaccines') or (hub == 'Tatuapé' and bu == 'vaccines')  or (hub == 'Curitiba' and bu == 'vaccines') or (hub == 'Campinas' and bu == 'vaccines') or (hub == 'Recife' and bu == 'vaccines'):
+            duração = 40
+        elif  (hub == 'São Cristóvão' and bu == 'laboratories') or (hub == 'Barra' and bu == 'laboratories') or (hub == 'Alphaville' and bu == 'laboratories') or (hub == 'Vila Olímpia' and bu == 'laboratories') or (hub == 'São Bernardo do Campo' and bu == 'laboratories') or (hub == 'Tatuapé' and bu == 'laboratories'):
+            duração = 30
+        elif (hub == 'Vitória' and bu == 'vaccines') or (hub == 'Brasília' and bu == 'laboratories') or (hub == 'Vitória' and bu == 'laboratories') or (hub == 'Curitiba' and bu == 'laboratories') or (hub == 'Recife' and bu == 'laboratories') or (hub == 'Campinas' and bu == 'laboratories'):
+            duração = 60
+        return duração    
 
-# response = requests.post(url=url, json=payload)
-# print(response.text)    
+    def regime(self, escala):
+        if (escala == 'VAC Técnica P1') or (escala == 'VAC Técnica P2') or (escala == 'LAB Técnica P1') or (escala == 'LAB Técnica P2'):
+            regime = 'rotating'
+        elif (escala == 'VAC Técnica D') or (escala == 'LAB Técnica D'):
+            regime = 'diarist'
+        return regime
+
+    def inserirdados(self, idslot, data, horario, parceiro, hub, regime, produto, id_tecnica, tecnica):
+        df = pd.DataFrame([[idslot, data, horario, parceiro, hub, regime, produto, id_tecnica, tecnica]], columns=['id_slot', 'data', 'horario', 'area', 'hub', 'regime', 'produto', 'id_tecnica', 'tecnica'])
+        df.to_sql(con=self.conexão, name='slots_abertos', schema='workstation', if_exists='append', method='multi', index=False)
+        consulta = f"select * from workstation.slots_abertos"
+        tabela = pd.read_sql_query(consulta, con=self.conexão)
+        print('Adicionando dados na tabela...')
+        return tabela
+
+    def token(self):
+        consulta = f"""
+        select remember_token from users where username = 'gabriel.rangel@beepsaude.com.br'
+        """
+        self.tkn = pd.read_sql_query(consulta, con=self.serverproduction)
+        self.tkn = self.tkn.iloc[0]['remember_token']
+        self.url = f'https://api.beepapp.com.br/api/v8/booking_management/schedule_bookings?session_token={self.tkn}'
+        return self.url
+
+# dash = Dashboard()
+# dash.tratarcapacidade('2022-10-10', 'Rio de Janeiro', 'vaccines')
